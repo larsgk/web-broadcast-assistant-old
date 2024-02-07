a19f224eb86856d09fd311809c1e27a5a58a7263
Lars Knudsen <larsgk@gmail.com>
2024-02-07 09:13:25 +0100

    Adding initial COBS lib

---
+++ web/lib/helpers.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+export const compareTypedArray = (arr1, arr2) => {
+        if (arr1.constructor.name !== arr2.constructor.name) {
+                throw new Error(`Inputs must be of same type (${arr1.constructor.name} != ${arr2.constructor.name})`);
+        }
+
+        if (arr1.length !== arr2.length) {
+                return false;
+        }
+
+        return arr1.every((value, index) => value === arr2[index]);
+}
+
+export const arrayToHex = arr => {
+        return Array.from(arr).map((b) => b.toString(16).padStart(2, "0")).join(', ');
+}
