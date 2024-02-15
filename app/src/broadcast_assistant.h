7b81586c7c8066ec0ce25cb8605b838b7310ff9b
Jens Rehhoff Thomsen <jthm@demant.com>
2024-02-15 15:37:36 +0100

    Append RSSI and LE BT address message data
    Broadcast assistant app will append RSSI and LE BT address information
    to the webusb message

--- app/src/broadcast_assistant.h
+++ app/src/broadcast_assistant.h
@@ -12,6 +12,9 @@
 #define __BROADCAST_ASSISTANT_H__
 
 #include <zephyr/types.h>
+#include <zephyr/bluetooth/gap.h>
+
+#define BT_DATA_RSSI (BT_DATA_MANUFACTURER_DATA - 1)
 
 void scan_for_broadcast_source(uint8_t seq_no);
 void scan_for_broadcast_sink(uint8_t seq_no);
